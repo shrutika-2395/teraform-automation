ccc637f78d55e94b60f0b57a880f05fd311c5783
shrutika-2395 <shrutikashinde2395@gmail.com>
2025-04-05 14:27:21 +0400

    Update backend.tf
    bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-sample-batch-26"
+    bucket = "mydev-project-terraform-jenkins-server-shrutika"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
